0200b3b84b068d053b7d8604bfe2237e3db4e928
sianios <m@sianios.com>
2015-01-08 13:46:45 +0200

    new ip method

--- do_dns.sh
+++ do_dns.sh
@@ -1,11 +1,18 @@
 #!/bin/bash 
 # Updates A records of a domain using Digital Ocean APIv2.
-# Install curl before running.
 
 api_key="your_key"
-domain="yourdoamin.com"
-record_id="your_record_id"
+domain="yourdomain.com"
+record="your_record_id"
+ip="$(curl -s --connect-timeout 15 http://checkip.dyndns.org | sed -e \
+    's/.*Current IP Address: //' -e 's/<.*$//' | egrep -o \
+    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
 
-ip="$(curl -s ifconfig.me)"
+if [ -z $ip ]; then
+    ip="$(curl -s --connect-timeout 15 ifconfig.me/ip)"
+fi
+
 echo $ip
-curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" -d '{"data":"'"$ip"'"}' "https://api.digitalocean.com/v2/domains/$domain/records/$record_id"
+curl -X PUT -H "Content-Type: application/json" \
+-H "Authorization: Bearer $api_key" -d '{"data":"'"$ip"'"}' \
+"https://api.digitalocean.com/v2/domains/$domain/records/$record"
