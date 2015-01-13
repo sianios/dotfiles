6b8046bcfbacaf5f7126688fab24c69bbba69179
sianios <m@sianios.com>
2015-01-13 22:42:59 +0200

    extract compressed files

---
+++ eac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -f "$1" ]; then
+    case "$1" in
+        *.tar.gz)   tar xzvf "$1" ;;
+        *.tar.bz2)  tar xvzf "$1" ;;
+        *.zip)      unzip "$1" ;;
+        *.rar)      unrar x "$1" ;;
+        *.7z)     7z x "$1" ;;
+        *)          echo "Not able to extract $1" ;;
+    esac
+fi
