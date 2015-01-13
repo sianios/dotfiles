1b9366dd546445576d3710ce8bf7578b46414c6f
sianios <m@sianios.com>
2015-01-13 22:49:22 +0200

    noth

--- eac.sh
+++ eac.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Extracts the following
 if [ -f "$1" ]; then
     case "$1" in
         *.tar.gz)   tar xzvf "$1" ;;
         *.tar.bz2)  tar xvzf "$1" ;;
         *.zip)      unzip "$1" ;;
         *.rar)      unrar x "$1" ;;
-        *.7z)     7z x "$1" ;;
+        *.7z)       7z x "$1" ;;
         *)          echo "Not able to extract $1" ;;
     esac
 fi
