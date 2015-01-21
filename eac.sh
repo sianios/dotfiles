f7238dc5f4b7ebc4440892f3d9adf0d8039180d6
sianios <m@sianios.com>
2015-01-21 21:17:43 +0200

    .tar.bz2 fix

--- eac.sh
+++ eac.sh
@@ -3,9 +3,9 @@
 if [ -f "$1" ]; then
     case "$1" in
         *.tar.gz)   tar xzvf "$1" ;;
-        *.tar.bz2)  tar xvzf "$1" ;;
+        *.tar.bz2)  tar xvjf "$1" ;;
         *.zip)      unzip "$1" ;;
-        *.rar)      unrar x "$1" ;;
+        *.rar)      unrar x "$1" ;
         *.7z)       7z x "$1" ;;
         *.gz)       gzip -dk "$1" ;;
         *.xz)       xz -dk "$1" ;;
