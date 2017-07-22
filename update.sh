1079e1f182a10ca93d71104c36eaab4b67e04454
sianios <m@sianios.com>
2017-07-22 17:42:49 +0300

    removed update pepper

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ menu="1 - Update
 2 - Upgrade
 3 - Dist-Upgrade
 L - List Available Upgrades
-F - Update Pepper Flash
 A - apt Autoremove
 O - Remove Obsolete Packages
 D - Purge Packages using deborphan
@@ -18,11 +17,12 @@ while true; do
     read option
     case $option in
         1) sudo apt-get update ;;
-        2) sudo apt-get upgrade ;;
+        2) sudo apt-get upgrade -V ;;
         3) sudo apt-get dist-upgrade ;;
         l|L) apt list --upgradable 
             read -p "Press any key to continue" ;;
-        f|F) sudo update-pepperflashplugin-nonfree --install ;;
+#        f|F) sudo update-pepperflashplugin-nonfree --install ;;
+#        #Package was removed in Debian 9 so this is useless
         a|A) sudo apt-get autoremove ;;
         o|O) sudo aptitude purge ~o ;;
         d|D) sudo apt-get purge $(deborphan) ;; #deborphan package must be installed
